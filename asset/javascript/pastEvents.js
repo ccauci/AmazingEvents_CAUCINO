57f65b0f74eccabd87c3872bbed86c6d997383d7
Carolina <carolina.caucino@gmail.com>
2023-03-27 09:00:08 -0300

    commitdos

--- asset/javascript/pastEvents.js
+++ asset/javascript/pastEvents.js
@@ -59,33 +59,3 @@ contenedorCheck.addEventListener("change", () => {
 
 
 
-
-
-
-
-// import data from "../javascript/amazing.js";
-
-// const fecha = data.currentDate;
-
-// let { events } = data;
-
-// let past = events.filter(evento=>evento.date<fecha);
-// console.table(past);
-
-// const template = document.querySelector(".plantilla").content; 
-// const padre = document.querySelector(".card-tod"); 
-// const fragment = document.createDocumentFragment();
-
-// past.forEach((event) => {
-
-// template.querySelector(".card-title").textContent = event.name; 
-// template.querySelector(".card-text").textContent = event.description;
-// template.querySelector(".card-img-top").src = event.image;
-// template.querySelector(".card-price").textContent = event.price;
-
-// const copia = template.cloneNode(true);
-// fragment.appendChild(copia);
-
-// });
-
-// padre.appendChild(fragment);
